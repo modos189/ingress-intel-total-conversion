703bcf5619e52237d8b6deae2f5e456f62f7830d
modos189 <git@modos189.ru>
2022-07-29 12:53:55 +0300

    test pr: Removing the PR build from the site after a PR merge

--- plugins/basemap-stamen.js
+++ plugins/basemap-stamen.js
@@ -4,39 +4,42 @@
 // @version        0.2.1
 // @description    Add the 'Toner' and 'Watercolor' map layers from maps.stamen.com.
 
-
 // see API here http://maps.stamen.com/
 // https://stamen-maps.a.ssl.fastly.net/js/tile.stamen.js (overcomplicated)
 
-function setup () {
-  var baseUrl = 'https://stamen-tiles-{s}.a.ssl.fastly.net/{layer}/{z}/{x}/{y}.{type}';
+function setup() {
+  var baseUrl =
+    "https://stamen-tiles-{s}.a.ssl.fastly.net/{layer}/{z}/{x}/{y}.{type}";
   var L_StamenTileLayer = L.TileLayer.extend({
     options: {
-      subdomains: 'abcd',
-      type: 'png',
+      subdomains: "abcd",
+      type: "png",
       minZoom: 0,
       maxZoom: 21,
       attribution: [
         'Map tiles by <a href="http://stamen.com/">Stamen Design</a>, ',
         'under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. ',
         'Data by <a href="http://openstreetmap.org/">OpenStreetMap</a>, ',
-        'under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
-      ].join('')
+        'under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.',
+      ].join(""),
     },
     initialize: function (name, options) {
-      options.layer = name.replace(' ','-').toLowerCase();
+      options.layer = name.replace(" ", "-").toLowerCase();
       L.TileLayer.prototype.initialize.call(this, baseUrl, options);
-    }
+    },
   });
 
-  function addLayer (name,options) {
-    layerChooser.addBaseLayer(new L_StamenTileLayer(name,options),'Stamen ' + name);
+  function addLayer(name, options) {
+    layerChooser.addBaseLayer(
+      new L_StamenTileLayer(name, options),
+      "Stamen " + name
+    );
   }
 
   var options = { minZoom: 0, maxNativeZoom: 20 };
-  addLayer('Toner',options);
-  addLayer('Toner Background',options);
-  addLayer('Toner Lite',options);
+  addLayer("Toner", options);
+  addLayer("Toner Background", options);
+  addLayer("Toner Lite", options);
   // transparent layers. could be useful over satellite imagery or similar
   // addLayer('Toner Hybrid',options);
   // addLayer('Toner Labels',options);
@@ -52,13 +55,13 @@ function setup () {
     minZoom: 1,
     maxZoom: 21,
     maxNativeZoom: 18,
-    type: 'jpg',
+    type: "jpg",
     attribution: [
       'Map tiles by <a href="http://stamen.com/">Stamen Design</a>, ',
       'under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. ',
       'Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, ',
-      'under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
-    ].join('')
+      'under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.',
+    ].join(""),
   };
-  addLayer('Watercolor',options);
+  addLayer("Watercolor", options);
 }
